cc2ade52f8f7448d6add378039afd2305b154e03
moliloo <dev.murilobrito@gmail.com>
2024-07-04 17:46:50 -0300

    fix item consumable data structure

--- module/data/item/item-consumable.mjs
+++ module/data/item/item-consumable.mjs
@@ -4,17 +4,16 @@ const fields = foundry.data.fields
 
 export default class AstroprismaConsumable extends AstroprismaItemBase {
 	static defineSchema() {
-		const requiredInteger = { required: true, nullable: false, integer: true }
-		const parentSchema = super.defineSchema()
-		const schema = {
-			...parentSchema,
-			price: new fields.SchemaField({
-				value: new fields.NumberField({ ...requiredInteger, initial: 0, min: 0 }),
-			}),
-			quantity: new fields.SchemaField({
-				value: new fields.NumberField({ ...requiredInteger, initial: 1, min: 0 }),
-			}),
-		}
+      const requiredInteger = { required: true, nullable: false, integer: true }
+		const schema = super.defineSchema()
+
+		schema.price = new fields.SchemaField({
+			value: new fields.NumberField({ ...requiredInteger, initial: 0, min: 0 }),
+		})
+
+		schema.quantity = new fields.SchemaField({
+			value: new fields.NumberField({ ...requiredInteger, initial: 1, min: 0 }),
+		})
 
 		return schema
 	}
